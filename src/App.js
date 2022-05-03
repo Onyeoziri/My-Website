115fa7fc65d3f6754ce51766af473b8d7da20b5d
Onyeoziri I <Onyeoziri@gmail.com>
2022-05-03 08:31:05 -0400

    added react Dom

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Learn React
+            Learn React
           </p>
       </header>
 
